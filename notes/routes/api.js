01e81dd29a877eba104dcbe4322d10a655db9061
Thiago Senador <TSiqueira2@slb.com>
2017-02-27 18:25:08 -0600

    adding google datastore

---
+++ notes/routes/api.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+var datastore = require('@google-cloud/datastore')({
+  projectId: 'notesintheair-160023',
+  keyFilename: './private/notesintheair_key.json'
+});
+
+/* GET home page. */
+router.get('/api', function (req, res, next) {
+  // Save data to Datastore. 
+  var blogPostData = {
+    title: 'How to make the perfect homemade pasta',
+    author: 'Andrew Chilton',
+    isDraft: true
+  };
+
+  var blogPostKey = datastore.key('BlogPost');
+
+  datastore.save({
+    key: blogPostKey,
+    data: blogPostData
+  }, function (err) {
+    // `blogPostKey` has been updated with an ID so you can do more operations 
+    // with it, such as an update. 
+    blogPostData.isDraft = false;
+
+    datastore.save({
+      key: blogPostKey,
+      data: blogPostData
+    }, function (err) {
+      if (!err) {
+        // The blog post is now published! 
+      }
+    });
+  });
+  res.json({ message: 'thiago rocksssss!!!' })
+});
+
+module.exports = router;
