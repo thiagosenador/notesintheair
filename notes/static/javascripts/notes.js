d248a7bf246cdc52946b5baba3752e10167aaf4a
Thiago Senador <TSiqueira2@slb.com>
2017-04-05 19:38:00 -0500

    fixing issues

--- notes/static/javascripts/notes.js
+++ notes/static/javascripts/notes.js
@@ -9,23 +9,28 @@
     createNoteForm.addEventListener('submit', e => {
         e.preventDefault();
 
-        var data = {};
-        data['note'] = noteTxt.value;
-        data['lat'] = latTxt.value;
-        data['lng'] = lngTxt.value;
+        firebase.auth().currentUser.getToken(true).then(function (idToken) {
+            var data = {};
+            data['note'] = noteTxt.value;
+            data['lat'] = latTxt.value;
+            data['lng'] = lngTxt.value;
+            data['user'] = firebase.auth().currentUser.uid;
 
-        console.log(data);
-
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', '/api/create_note', true);
-        xhr.setRequestHeader("Content-type", "application/json");
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                var json = JSON.parse(xhr.responseText);
-                console.log(json.email + ", " + json.password)
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', '/api/create_note', true);
+            xhr.setRequestHeader('Authorization', 'idToken');
+            xhr.setRequestHeader('Content-type', 'application/json');
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    var json = JSON.parse(xhr.responseText);
+                    console.log(json.email + ", " + json.password)
+                }
             }
-        }
 
-        xhr.send(JSON.stringify(data));
+            xhr.send(JSON.stringify(data));
+
+        }).catch(function (error) {
+            console.log('Error... ' + error);
+        });
     });
 }());
--- notes/static/javascripts/services/notes_services.js
+++ notes/static/javascripts/services/notes_services.js
@@ -1,16 +1,23 @@
 const datastore = require('../datastore');
+const admin = require('firebase-admin');
 
 var notes = {
     createNote: function (req, res) {
-        var note = {
-            content: req.body['note'],
-            user: 'Thiago',
-            lat: req.body['lat'],
-            lng: req.body['lng'],
-            date: Date.now()
-        }
+        admin.auth().verifyIdToken(req.headers['authorization'])
+            .then(function () {
+                var note = {
+                    content: req.body['note'],
+                    user: req.body['user'],
+                    lat: req.body['lat'],
+                    lng: req.body['lng'],
+                    date: Date.now()
+                }
 
-        // datastore.save('Note', note);
+                datastore.save('Note', note);
+
+            }).catch(function (error) {
+                console.error(error);
+            });
     }
 };
 
